438765b211d5039aad01a5f831870dadee1a28f0
Jonas Adler <jonas@toyrocketscience.com>
2018-01-25 17:39:17 +0100

    Improve authentication

--- src/main/scala/io/trsc/http4stryout/AuthenticationService.scala
+++ src/main/scala/io/trsc/http4stryout/AuthenticationService.scala
@@ -1,21 +1,24 @@
 package io.trsc.http4stryout
 
 import cats.Monad
+import cats.implicits._
 import cats.data.{Kleisli, OptionT}
 import io.trsc.http4stryout.CtpModel.TokenMeta
 import org.http4s.headers.Authorization
-import org.http4s.{AuthScheme, Credentials, HttpService}
+import org.http4s.{AuthScheme, Credentials, HttpService, Response, Status}
 
 class AuthenticationService[F[_]: Monad](ctpClient: CtpClient[F]) {
 
   def secure(service: HttpService[F]): HttpService[F] = Kleisli { request ⇒
-    for {
+    val authorized = for {
       header                                      ← OptionT.fromOption[F](request.headers.get(Authorization))
       Credentials.Token(AuthScheme.Bearer, token) ← OptionT.pure[F](header.credentials)
       TokenMeta(true, _, Some(exp))               ← OptionT(ctpClient.introspect(token))
       if exp > System.currentTimeMillis()
       response ← service(request)
     } yield response
+
+    authorized <+> OptionT.pure[F](Response[F](Status.Unauthorized))
   }
 
 }
